f2dec994803983bedd3fcb1fea19c28254f465a9
Pushkar Choudhary <96918138+Pushkariiit@users.noreply.github.com>
2024-11-25 00:54:49 +0530

    backend

--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const couponController = require('../controllers/couponController');
+const mockDB = require("../config/mockDB");
 
 router.post('/generate', couponController.generateCoupon);
 router.post('/validate', couponController.validateCoupon);
+
+router.get("/logs", (req, res) => {
+    res.status(200).json({
+        message: "Request logs retrieved successfully",
+        logs: mockDB.logs
+    });
+});
 
 module.exports = router;
